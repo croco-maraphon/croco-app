dfe3be95c5957fba54ba819648c7d9abc0508b93
esplusnin <esplusnin94@yandex.ru>
2023-04-17 23:05:43 +0400

    Screen complete

---
+++ croco/Screens/Teams/TeamsView.swift
@@ -0,0 +1,80 @@
+//
+//  TeamsView.swift
+//  croco
+//
+//  Created by Евгений on 17.04.2023.
+//
+
+import UIKit
+
+final class TeamsView {
+    static let shared = TeamsView()
+    
+    lazy var backgroundImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "background")
+        
+        return imageView
+    }()
+    
+    lazy var topTeamView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(red: 255, green: 255, blue: 255, alpha: 1)
+        view.layer.cornerRadius = 10
+        view.layer.masksToBounds = true
+        
+        return view
+    }()
+    
+    lazy var topTeamImage: UIImageView = {
+        var imageView = UIImageView()
+        imageView.image = UIImage(named: "Cowboy")
+        
+        return imageView
+    }()
+    
+    lazy var topTeamLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Ковбои"
+        label.font = UIFont.systemFont(ofSize: 20)
+        
+        return label
+    }()
+    
+    lazy var bottomTeamView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(red: 255, green: 255, blue: 255, alpha: 1)
+        view.layer.cornerRadius = 10
+        view.layer.masksToBounds = true
+        
+        return view
+    }()
+    
+    lazy var bottomTeamImage: UIImageView = {
+        var imageView = UIImageView()
+        imageView.image = UIImage(named: "Burger")
+        
+        return imageView
+    }()
+    
+    lazy var bottomTeamLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Стройняшки"
+        label.font = UIFont.systemFont(ofSize: 20)
+        
+        return label
+    }()
+    
+    lazy var readyButton: UIButton = {
+        var button = UIButton(type: .system)
+        button.setTitle("Игроки готовы", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = UIColor(hexString: "74A730")
+        
+        button.layer.cornerRadius = 10
+        button.layer.masksToBounds = true
+        
+        return button
+    }()
+}
