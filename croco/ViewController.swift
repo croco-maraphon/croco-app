4288dccdde4b325263689e8dcc798e4e3394102d
a.g.menshikov <a.g.menshikov@tinkoff.ru>
2023-04-17 10:34:43 +0300

    Initial Commit

---
+++ croco/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  croco
+//
+//  Created by Aleksandr Menshikov on 17.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
