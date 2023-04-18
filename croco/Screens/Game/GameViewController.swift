cba0cdde99cbf5060eda1fd958b6ce37527fa3fb
Ilyas Tyumenev <tumenoff@mail.ru>
2023-04-18 19:17:29 +0300

    Game screen (#17)
    * Create view on GameViewController
    
    * Add presenterSingleton
    
    ---------
    
    Co-authored-by: a.g.menshikov <a.g.menshikov@tinkoff.ru>

--- croco/Screens/Game/GameViewController.swift
+++ croco/Screens/Game/GameViewController.swift
@@ -11,28 +11,86 @@ import SwiftUI
 import SnapKit
 
 class GameViewController: UIViewController {
+    
+    private let gameView = GameView()
+    
+    let label = CategoriesPresenter.shared.getWordToPlay()
+    
+    var timer = Timer()
+    var secondRemaining = 60
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         self.navigationItem.setHidesBackButton(true, animated: true)
-        setBackground()
-        setTitle()
+        
+        setGameView()
+        startTimer()
+        correctButtonPressed()
+        wrongButtonPressed()
+        mainButtonPressed()
     }
     
-    public func setBackground() {
-        self.view.backgroundColor = .white
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        gameView.frame = view.bounds
+        view.addSubview(gameView)
     }
-    
-    public func setTitle() {
-        let label = UILabel()
-        label.text = "Game View Controller"
         
-        self.view.addSubview(label)
+    public func setGameView() {
+        self.view.addSubview(gameView)
         
-        label.snp.makeConstraints { make in
-            make.center.equalTo(self.view)
+        gameView.snp.makeConstraints { make in
+            make.top.bottom.leading.trailing.equalToSuperview()
         }
     }
- 
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1.0,
+                                     target: self,
+                                     selector: #selector(updateTimer),
+                                     userInfo: nil,
+                                     repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        if secondRemaining > 10 {
+            secondRemaining -= 1
+            gameView.timerLabel.text = "00:\(secondRemaining)"
+        } else if secondRemaining > 0 {
+            secondRemaining -= 1
+            gameView.timerLabel.text = "00:0\(secondRemaining)"
+        }
+    }
+    
+    
+    // переход на CorrectViewController
+    private func correctButtonPressed() {
+        gameView.correctButton.addTarget(self, action: #selector(navigateToCorrect), for: .touchUpInside)
+        
+    }
+    
+    @objc func navigateToCorrect() {
+        MainCoordinator.shared.pop()
+    }
+    
+    // переход на WrangViewController
+    private func wrongButtonPressed() {
+        gameView.wrongButton.addTarget(self, action: #selector(navigateToWrong), for: .touchUpInside)
+        
+    }
+    
+    @objc func navigateToWrong() {
+        MainCoordinator.shared.pop()
+    }
+    
+    // переход на MainViewController
+    private func mainButtonPressed() {
+        gameView.mainButton.addTarget(self, action: #selector(navigateToMain), for: .touchUpInside)
+        
+    }
+    
+    @objc func navigateToMain() {
+        MainCoordinator.shared.pop()
+    }
 }
