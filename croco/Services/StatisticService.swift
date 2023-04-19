d73bb35fa4e4ec33c04b8709c70ec01df68fadd9
Ramil <r-yy@outlook.com>
2023-04-20 01:25:00 +0300

    Fix statistic service

--- croco/Services/StatisticService.swift
+++ croco/Services/StatisticService.swift
@@ -7,49 +7,70 @@
 
 import Foundation
 
-final class StatisticService: StatisticServiceProtocol {
+final class StatisticService {
     private let userDefaults = UserDefaults.standard
+    private let prefix = "croco "
 
-    public func getPointsFrom(team: String) -> Int {
-        userDefaults.integer(forKey: "croco \(team)")
-    }
+    public func getLeaderboard() -> [String: Int] {
+        var leaderboard: [String: Int] = [:]
+        let dictionary = userDefaults.dictionaryRepresentation()
 
-    public func setPointsTo(team: String, points: Int) {
-        userDefaults.set(points, forKey: "croco \(team)")
-    }
-
-    public func getDictionary() -> [String: Int] {
-        let dictionary = UserDefaults.standard.dictionaryRepresentation()
-
-        let filteredDictionary = dictionary.filter { (key, value) -> Bool in
-            return key.hasPrefix("croco ")
-        }
-
-        let prefixToRemove = "croco "
-        var updatedDictionary: [String: Int] = [:]
-
-        filteredDictionary.forEach { (key, value) in
-            if let intValue = value as? Int {
-                if key.hasPrefix(prefixToRemove) {
-                    let newKey = String(key.dropFirst(prefixToRemove.count))
-                    updatedDictionary[newKey] = intValue
-                } else {
-                    updatedDictionary[key] = intValue
+        for key in dictionary.keys {
+            if key.hasPrefix(prefix) {
+                if let value = dictionary[key] as? Int {
+                    let index = String(key.dropFirst(prefix.count))
+                    leaderboard[index] = value
                 }
             }
         }
+        return leaderboard
+    }
 
-        return updatedDictionary
+    public func sendGameResult(teams: [TeamModel]) {
+        let leaderboard = getLeaderboard()
+
+        for team in teams {
+            if leaderboard.keys.contains(team.teamName) {
+                let score = team.teamScore + userDefaults.integer(
+                    forKey: prefix + team.teamName
+                )
+                userDefaults.set(
+                    score,
+                    forKey: prefix + team.teamName
+                )
+            } else {
+                userDefaults.set(
+                    team.teamScore,
+                    forKey: prefix + team.teamName
+                )
+            }
+        }
     }
 
     public func restore() {
         let keys = userDefaults.dictionaryRepresentation().keys
 
         for key in keys {
-            if key.hasPrefix("croco ") {
+            if key.hasPrefix(prefix) {
                 userDefaults.removeObject(forKey: key)
             }
         }
         userDefaults.synchronize()
+    }
+
+    public func rename(team: TeamModel, newName: String) {
+        let leaderboard = getLeaderboard()
+
+        if leaderboard.keys.contains(team.teamName) {
+            let score = userDefaults.integer(
+                forKey: prefix + team.teamName
+            )
+            userDefaults.removeObject(
+                forKey: prefix + team.teamName
+            )
+            userDefaults.set(score, forKey: prefix + newName)
+        } else {
+            fatalError("Team not found")
+        }
     }
 }
