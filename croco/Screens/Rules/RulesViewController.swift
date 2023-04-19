9fb03606dd830872fd2bf1a3425e49284e5518c2
Privetyanikita <korniuk.mykyta@inbox.dsum.edu.ua>
2023-04-19 13:00:17 +0200

    add basic rules screen

--- croco/Screens/Rules/RulesViewController.swift
+++ croco/Screens/Rules/RulesViewController.swift
@@ -12,27 +12,47 @@ import SnapKit
 
 
 class RulesViewController: UIViewController {
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        setBackground()
-        setTitle()
-    }
-    
+    let textView = UITextView()
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            setBackground()
+            setupViews()
+            setupConstraints()
+        }
+        
+        func setupViews() {
+            view.addSubview(textView)
+            
+            textView.text = "В игру играют командами из двух или более человек.\n\nЗадача каждого игрока команды - объяснить слово, которое он видит на экране, следуя условиям, которые дополнительно указаны под загаданным словом.\n\nЧем больше слов отгадала команда, тем больше она заработает баллов. \n\nВыигрывает команда, набравшая больше всего баллов. На отгадывание слова дается одна минуту.\n\nПри нарушении правил объяснения слова, ход передается следующей команде. \n\n... \n...\n В игру играют командами из двух или более человек.\n\nЗадача каждого игрока команды - объяснить слово, которое он видит на экране, следуя условиям, которые дополнительно указаны под загаданным словом.\n\nЧем больше слов отгадала команда, тем больше она заработает баллов. \n\nВыигрывает команда, набравшая больше всего баллов. На отгадывание слова дается одна минуту.\n\nПри нарушении правил объяснения слова, ход передается следующей команде. \n\n... \n...\nВ игру играют командами из двух или более человек.\n\nЗадача каждого игрока команды - объяснить слово, которое он видит на экране, следуя условиям, которые дополнительно указаны под загаданным словом.\n\nЧем больше слов отгадала команда, тем больше она заработает баллов. \n\nВыигрывает команда, набравшая больше всего баллов. На отгадывание слова дается одна минуту.\n\nПри нарушении правил объяснения слова, ход передается следующей команде. \n\n... \n...\n"
+            textView.font = UIFont.systemFont(ofSize: 18)
+            textView.textColor = .black
+            textView.isEditable = false
+            textView.isScrollEnabled = true
+            textView.isSelectable = false
+            textView.backgroundColor = .clear
+        }
+        
+        func setupConstraints() {
+            textView.snp.makeConstraints { (make) in
+                make.edges.equalTo(view.safeAreaLayoutGuide).inset(8)
+            }
+        }
     public func setBackground() {
-        self.view.backgroundColor = .white
-    }
-    
-    public func setTitle() {
-        let label = UILabel()
-        label.text = "Rules View Controller"
+        let background = UIImage(named: "background")
         
-        self.view.addSubview(label)
+        guard let background = background else {
+            fatalError("background image is empty");
+        }
         
-        label.snp.makeConstraints { make in
-            make.center.equalTo(self.view)
+        let imageView = UIImageView(image: background)
+        
+        self.view.addSubview(imageView)
+        
+        imageView.snp.makeConstraints { make in
+            make.top.bottom.left.right.equalTo(view)
         }
     }
- 
 }
+
