6b7d65e1698553af48e133148e98c2ed6dcf0361
Ilyas Tyumenev <tumenoff@mail.ru>
2023-04-20 19:17:14 +0300

    Update Team screen (#37)
    Синхронизировались по TeamModel
    Реализовал добавление команды
    Поправил генератор эмодзи и команд

--- croco/Screens/Teams/TeamsViewController.swift
+++ croco/Screens/Teams/TeamsViewController.swift
@@ -10,15 +10,17 @@ import SnapKit
 
 class TeamsViewController: UIViewController {
     
-    private let team = TeamAPI.getTeam()
+    private var team = TeamAPI.getTeams()
+    private let newTeam = TeamAPI.addTeam()
     private let teamsTable: TeamsTableView = TeamsTableView()
-
+    
     // MARK: - Override Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         registerCells()
         configureViewController()
         conformProtocols()
+        addTeamButtonPressed()
         playersReadyButtonPressed()
     }
     
@@ -61,6 +63,27 @@ class TeamsViewController: UIViewController {
     @objc func navigateToCategories() {
         MainCoordinator.shared.push(.Categories)
     }
+    
+    // добавление команды
+    private func addTeamButtonPressed() {
+        teamsTable.addTeamButton.addTarget(self, action: #selector(addTeam), for: .touchUpInside)
+    }
+    
+    @objc func addTeam() {
+        if team.count < 3 {
+            team.append(newTeam[0])
+            teamsTable.tableView.beginUpdates()
+            teamsTable.tableView.insertRows(at: [(NSIndexPath(row: team.count - 1, section: 0) as IndexPath)],
+                                            with: .automatic)
+            teamsTable.tableView.endUpdates()
+        } else if team.count < 4 {
+            team.append(newTeam[1])
+            teamsTable.tableView.beginUpdates()
+            teamsTable.tableView.insertRows(at: [(NSIndexPath(row: team.count - 1, section: 0) as IndexPath)],
+                                            with: .automatic)
+            teamsTable.tableView.endUpdates()
+        }
+    }
 }
 
 // MARK: - UITableViewDataSource
@@ -82,6 +105,20 @@ extension TeamsViewController: UITableViewDataSource {
 extension TeamsViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 96
+        return 124
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        let canDelete = team.count > 2
+        return canDelete
+    }
+    
+    func tableView(tableView: UITableView,
+                   commitEditingStyle editingStyle: UITableViewCell.EditingStyle,
+                   forRowAtIndexPath indexPath: IndexPath) {
+        team.remove(at: indexPath.row)
+        teamsTable.tableView.beginUpdates()
+        teamsTable.tableView.deleteRows(at: [indexPath], with: .automatic)
+        teamsTable.tableView.endUpdates()
     }
 }
